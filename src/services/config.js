0272026bdb81b06f93fb0b4380307da7df2fd031
Valentyn Kuzmenko <valiktyno@gmail.com>
2021-03-31 21:09:13 -0400

    a

--- src/services/config.js
+++ src/services/config.js
@@ -5,7 +5,12 @@
 
 // Configuration Firebase (mettez-y les vôtres !)
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyB9HaUZwAOXKTHmh9XYPljJhejAaK-U5_U",
+  authDomain: "ex5-signets-622d1.firebaseapp.com",
+  projectId: "ex5-signets-622d1",
+  storageBucket: "ex5-signets-622d1.appspot.com",
+  messagingSenderId: "572663966715",
+  appId: "1:572663966715:web:e9ac23e10d4f2c5bb4395e"
 };
 export default firebaseConfig;
 
